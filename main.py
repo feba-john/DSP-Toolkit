538f6d55df57bf720439f95d1c67d55acabf9bd3
Feba Mary John <feba-mary.john@tu-ilmenau.de>
2026-04-16 00:15:57 +0200

    added noise signal function

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import matplotlib.pyplot as plt 
-from signal_generator import generate_sine
+import matplotlib.pyplot as plt
+from signal_generator import generate_sine, add_noise
 
-#parameters
+# parameters
 fs = 1000
 freq = 5
-duration  = 1
+duration = 1
 
-#function to generate sine wave
+# function to generate sine wave
 t, signal = generate_sine(freq, fs, duration)
 
-#plots
+# plots
 plt.plot(t, signal)
 plt.title("Sine Wave")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
+# plt.savefig("results/output.png")
 plt.show()
 
+noisy_signal = add_noise(signal, 0.5)
+plt.plot(t, noisy_signal)
+plt.title("Noisy signal")
+plt.savefig("results/output.png")
+plt.show()
--- signal_generator.py
+++ signal_generator.py
@@ -18,3 +18,19 @@ def generate_sine(freq, fs, duration):
     t = np.arange(0, duration, 1/fs)
     signal = np.sin(2 * np.pi * freq * t)
     return t, signal
+
+
+def add_noise(signal, noise_level):
+    """
+    add Gaussian (white) noise to sine signal generated
+
+    Args:
+        signal : Input signal array
+        noise_level (float): Scaling factor controlling noise amplitude
+
+    Returns:
+        Noisy signal obtained by adding Gaussian noise
+    """
+
+    noise = noise_level * np.random.randn(len(signal))
+    return signal + noise
